d0565c8089321b724d4ac52f029e99fbde17ac93
ivyhu <ajianouc@gmail.com>
2022-03-23 12:32:13 -0700

    added get and post request

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,25 +1,40 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-const { schema } = mongoose;
 
-mongoose.connect(`mongodb://localhost:${process.env.PORT}/${process.env.DB_NAME}`);
-console.log('db connection is',`mongodb://localhost:${process.env.PORT}/${process.env.DB_NAME}` );
+mongoose.connect(`mongodb://localhost:${process.env.MongoDB_PORT}/${process.env.DB_NAME}`);
+console.log('db connection is',`mongodb://localhost:${process.env.PORT}/${process.env.DB_NAME}`);
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
-const wordschema = new Schema({
+let wordSchema = new mongoose.Schema({
   word: { type: String, unique: true},
   definition: String,
   auther: { type: String, default: 'Jane'},
   date: { type: Date, default: Date.now }
 });
 
-const Words = mongoose.model('Words'. wordschema);
+let Words = mongoose.model('Words', wordSchema);
 
-const save((data) => {
-
+let save=((data) => {
+  console.log('saving data ', data);
+  let word = new Words(data);
+  return word.save((e) =>{
+    if(e){
+      console.log(e);
+    }
+  });
 });
+
+let getWords= async function() {
+  const data = await Words.find();
+  // console.log('data is ', data);
+  return data;
+};
+
+
+
 
 
 module.exports.save = save;
+module.exports.getWords = getWords;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const save = require("./db.js").save;
+const getWords = require("./db.js").getWords;
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+// set up get method and post method
+app.get('/words', (req, res) => {
+  // console.log('test');
+  return getWords()
+  .then(data => {
+    save(data);
+    res.send('success');
+  });
+
+});
+
+app.post('/words', (req, res) => {
+  let data = req.body;
+  console.log('data ', data);
+  save(data);
+  res.send('success');
+
+
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
