9454c29f1b8f05b2f6fd95b1819bc6b91c2f302a
ivyhu <ajianouc@gmail.com>
2022-03-23 10:24:13 -0700

    added database set up

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
+const { schema } = mongoose;
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+const wordschema = new Schema({
+  word: { type: String, unique: true},
+  definition: String,
+  auther: { type: String, default: 'Jane'},
+  date: { type: Date, default: Date.now }
+});
+
+const Words = mongoose.model('Words'. wordschema);
+
+const save((data) => {
+
+});
+
+
