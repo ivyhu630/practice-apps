ede80c37512bdb68ed9843bc698e1e0b5dd0da53
ivyhu <ajianouc@gmail.com>
2022-03-23 10:34:59 -0700

    added database connection

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 const { schema } = mongoose;
 
+mongoose.connect(`mongodb://localhost:${process.env.PORT}/${process.env.DB_NAME}`);
+console.log('db connection is',`mongodb://localhost:${process.env.PORT}/${process.env.DB_NAME}` );
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
@@ -19,3 +22,4 @@ const save((data) => {
 });
 
 
+module.exports.save = save;
