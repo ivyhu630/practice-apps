c28ae1fa7ceb58b103eb66817787aa596f607f0a
ivyhu <ajianouc@gmail.com>
2022-03-24 11:11:13 -0700

    refactored server index

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const save = require("./db.js").save;
-const update = require("./db.js").update;
-const deleteOne = require("./db.js").deleteOne;
-const getWords = require("./db.js").getWords;
+const routes = require("./routes.js");
+
 
 const app = express();
 
@@ -13,34 +11,13 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.json());
 
 // set up get method and post method
-app.get('/words', (req, res) => {
-  return getWords()
-  .then(data => {
-    res.send(data);
-  });
-});
+app.get('/words', routes.get);
 
-app.post('/edit', (req, res) => {
-  let data = req.body;
-  update(data)
-  .then(() =>{
-    res.send('success');
-  });
-});
+app.put('/edit', routes.put);
 
-app.post('/words', (req, res) => {
-  let data = req.body;
-  save(data).then(() =>{
-    res.send('success');
-  });
-});
+app.post('/words', routes.post);
 
-app.post('/delete', (req, res) => {
-  let id = req.body;
-  deleteOne(id).then(() =>{
-    res.send('success');
-  });
-});
+app.delete('/delete', routes.delete);
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
