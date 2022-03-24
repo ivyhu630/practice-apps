b5e032b02ee80e9e14bd16ef1c93ccd85b69e07e
ivyhu <ajianouc@gmail.com>
2022-03-24 10:16:58 -0700

    created dbHandler

--- 1-glossary/server/databaseHandler.js
+++ 1-glossary/server/databaseHandler.js
@@ -1,44 +1,22 @@
-let save= async function(data) {
-  // console.log('saving data ', data);
-  let word = new Words(data);
-  try{
-    var p = await word.save();
-    return p;
-  }  catch(e) {
-    console.log(e);
-  }
+const { mongoose, Words } = require(./db.js);
+
+module.exports = {
+  save: ({ word, definition }) => Words.create({ word, definition }),
+
+  update: ({ _id }, { word, definition }) => Words.update({ _id }, { word, definition }),
+
+    // const { word, definition } = data.newWord;
+    // try {
+    //   let p = await Words.where({ _id: data.oldWordID }).updateOne( { word, definition });
+    //   return p;
+    // } catch(e) {
+    //   console.log(e);
+    // }
+
+  deleteOne: ({ _id }) => Words.deleteOne({ _id }),
+
+  getWords: () => Words.find()
+
 };
 
-let update =  async function(data) {
-  const { word, definition } = data.newWord;
-  // console.log('updating ', data);
-  try {
-    let p = await Words.where({ _id: data.oldWordID }).updateOne( { word, definition });
-    return p;
-  } catch(e) {
-    console.log(e);
-  }
-};
 
-let deleteOne =  async function({id}) {
-  try {
-    let p = await Words.deleteOne({ "_id": id });
-    return p;
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-let getWords= async function() {
-  try {
-    const data = await Words.find();
-    return data;
-  } catch(e) {
-    console.log(e);
-  }
-};
-
-module.exports.save = save;
-module.exports.getWords = getWords;
-module.exports.update = update;
-module.exports.deleteOne = deleteOne;
